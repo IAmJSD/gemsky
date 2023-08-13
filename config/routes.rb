79c5d50abaad5d959c0ac68937c5b0a0ef37ce28
Astrid Gealer <astrid@gealer.email>
2023-08-13 13:58:12 +0100

    add authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  # Defines the authentication routes.
+  get "login", to: "authentication#login"
+  patch "login", to: "authentication#totp_handler"
+  post "login", to: "authentication#login_post"
+  delete "logout", to: "authentication#logout"
+  get "forgot_password", to: "authentication#forgot_password_enter_email"
+  post "forgot_password", to: "authentication#forgot_password_enter_email_post"
+  get "forgot_password/:token", to: "authentication#forgot_password_remainder"
+  post "forgot_password/:token", to: "authentication#forgot_password_remainder_post"
+  get "register", to: "authentication#register_enter_email"
+  post "register", to: "authentication#register_enter_email_post"
+  get "register/:token", to: "authentication#register_remainder"
+  post "register/:token", to: "authentication#register_remainder_post"
+  get "email/update/:token", to: "authentication#email_update_token_click"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_060421) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_064727) do
   create_table "half_tokens", force: :cascade do |t|
     t.string "token", null: false
     t.integer "user_id", null: false
@@ -20,12 +20,36 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_060421) do
     t.index ["user_id"], name: "index_half_tokens_on_user_id"
   end
 
+  create_table "new_user_email_confirmations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "totp_recovery_codes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_totp_recovery_codes_on_user_id"
+  end
+
+  create_table "user_email_update_requests", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "email", null: false
+    t.string "token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_user_email_update_requests_on_token", unique: true
+    t.index ["user_id"], name: "index_user_email_update_requests_on_user_id", unique: true
+  end
+
+  create_table "user_password_change_requests", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_user_password_change_requests_on_token", unique: true
+    t.index ["user_id"], name: "index_user_password_change_requests_on_user_id"
   end
 
   create_table "user_tokens", force: :cascade do |t|
@@ -44,10 +68,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_060421) do
     t.datetime "updated_at", null: false
     t.string "totp_secret"
     t.index "\"half_token\"", name: "index_users_on_half_token"
+    t.index "\"user_token\"", name: "index_users_on_user_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "half_tokens", "users"
   add_foreign_key "totp_recovery_codes", "users"
+  add_foreign_key "user_email_update_requests", "users"
+  add_foreign_key "user_password_change_requests", "users"
   add_foreign_key "user_tokens", "users"
 end
