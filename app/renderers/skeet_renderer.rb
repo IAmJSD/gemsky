b400e609d183512ee94da5bc1142cc264deba88a
Astrid Gealer <astrid@gealer.email>
2023-08-21 19:49:05 +0100

    facets

--- app/renderers/skeet_renderer.rb
+++ app/renderers/skeet_renderer.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-
 class SkeetRenderer
     CONTENT_WARNING = /^([ct]w: *.+?)\n/mi
-    SHOULD_TRUNCATE = /^(.{20}).+/
     MANY_MULTIPLE_NEWLINES = /\n{5,}/m
     BOLD_TEXT = /\*\*([^\n*]+?)\*\*/
     ITALIC_TEXT = /\*([^\n]+?)\*/
@@ -16,9 +13,10 @@ class SkeetRenderer
     YOUTUBE_VIDEO_ID = /^[a-zA-Z0-9_-]{11}$/
     TWITCH_PATH = /^(\/[a-zA-Z0-9][\w]{2,24})|(\/videos\/[0-9]+)$/
 
-    def initialize(text_content, skeet_id)
+    def initialize(text_content, skeet_id, facets)
         @text_content = text_content
         @skeet_id = ERB::Util.html_escape(skeet_id)
+        @facets = facets
     end
 
     def media_url
@@ -28,25 +26,36 @@ class SkeetRenderer
 
     def render(&block)
         # Handle content warnings.
-        html, closing, text = handle_content_warnings
+        html, closing, text, length = handle_content_warnings
 
-        # Parse the text to HTML first before we do anything else.
-        content_html = ERB::Util.html_escape(text)
+        # Remove any facets that are in the content warning.
+        @facets = @facets.select do |facet|
+            facet['index']['byteStart'] >= length
+        end
 
         # Append the body information to the HTML.
         html += '<div class="skeet-content">'
 
-        # Handle links.
-        content_html, no_mutation_ranges = handle_links(content_html)
+        # Tokenize the text and handle facets.
+        tokens = tokenize_facets(text)
+
+        # XSS escape the non-facet tokens.
+        tokens = tokens.map do |token|
+            if token.is_a?(FacetToken)
+                token
+            else
+                ERB::Util.html_escape(token)
+            end
+        end
 
         # Handle formatting.
-        content_html = handle_formatting(content_html, no_mutation_ranges)
+        handle_formatting(tokens)
 
         # Handle newlines.
-        content_html = handle_newlines(content_html)
+        handle_newlines(tokens)
 
         # Close the div.
-        html += content_html + '</div>'
+        html += tokens.map(&:to_s).join('') + '</div>'
 
         # Call the block with ourselves.
         res = block.call(self) if block
@@ -63,32 +72,17 @@ class SkeetRenderer
 
     private
 
-    def scan_with_index(string, regex)
-        res = []
-        string.scan(regex) do |c|
-            res << [$&, $~.offset(0)[0]]
-        end
-        res
-    end
-
     def handle_spooky_url(url)
         # Try to parse the URL.
         begin
             parsed = URI.parse(url)
         rescue StandardError
             # Nope
-            return false, '', '', false
+            return false, false
         end
 
         # Check if it is a safe URL scheme for the user to click.
-        return false, '', '', false unless ALLOWED_URL_SCHEMES.include?(parsed.scheme)
-
-        # Get the start fragment.
-        start_fragment = "#{parsed.scheme}://#{parsed.host}"
-
-        # Get the truncatable fragment.
-        truncatable_fragment = parsed.path
-        truncatable_fragment += "?#{parsed.query}" if parsed.query
+        return false, false unless ALLOWED_URL_SCHEMES.include?(parsed.scheme)
 
         # Check if this is a tenor or giphy link.
         case parsed.host
@@ -123,139 +117,129 @@ class SkeetRenderer
             is_media = false
         end
 
-        # Return the parsed URL, the start fragment, the truncatable fragment, and the media status.
-        [parsed, start_fragment, truncatable_fragment, is_media]
+        # Return the parsed URL.
+        [parsed, is_media]
     end
 
-    def handle_links(content_html)
-        # Find all the links.
-        links = scan_with_index(content_html, URI.regexp)
+    def handle_formatting(tokens)
+        tokens.each_with_index do |token, index|
+            next unless token.is_a?(String)
 
-        # Defines the array for no mutation ranges.
-        no_mutation_ranges = []
+            # Handle bold.
+            token = token.gsub(BOLD_TEXT, '<b>\1</b>')
 
-        # Loop through the links.
-        links.each_with_index do |link_a, array_index|
-            # Get the link and the index.
-            link, index = link_a
+            # Handle italic.
+            token = token.gsub(ITALIC_TEXT, '<i>\1</i>')
 
-            # Try to parse the link.
-            parsed, start_fragment, truncatable_fragment, is_media = handle_spooky_url(link)
+            # Handle underline.
+            token = token.gsub(UNDERLINE_TEXT, '<u>\1</u>')
 
-            # If it was not parsed, add the link start and end to the no mutation ranges.
-            unless parsed
-                no_mutation_ranges << [index, index + link.length]
-                next
-            end
+            # Handle strikethrough.
+            token = token.gsub(STRIKETHROUGH_TEXT, '<s>\1</s>')
 
-            # Truncate the fragment if it is too long.
-            truncatable_fragment = truncatable_fragment.gsub(SHOULD_TRUNCATE, '\1…')
-
-            # Build the HTML element.
-            html = "<a target=\"_blank\" href=\"#{link}\">#{start_fragment}#{truncatable_fragment}</a>".html_safe
-
-            # Replace the link with the HTML element without mutating the string.
-            content_html = content_html[0...index] + html + content_html[index + link.length..-1]
-
-            # Add the difference between the link and the a tag to every item in the array after this one.
-            difference = html.length - link.length
-            links[array_index + 1..-1].each do |link_b|
-                link_b[1] += difference
-            end
-
-            # Add to the no mutation ranges.
-            no_mutation_ranges << [index, index + html.length]
-
-            # If this is media, set @last_mmedia to this.
-            @last_media = link if is_media
+            # Set the token.
+            tokens[index] = token
         end
-
-        # Return the content HTML and the no mutation ranges.
-        [content_html, no_mutation_ranges]
     end
 
-    class NoFormatting
-        def initialize(text)
-            @text = text
+    def handle_newlines(tokens)
+        tokens.each_with_index do |token, index|
+            next unless token.is_a?(String)
+
+            # Replace multiple newlines with a couple newlines.
+            token = token.gsub(MANY_MULTIPLE_NEWLINES, '\n\n')
+
+            # Replace newlines with brs.
+            token = token.gsub(/\n/, '<br>')
+
+            # Set the token.
+            tokens[index] = token
+        end
+    end
+
+    class FacetToken
+        def initialize(html)
+            @html = html
         end
 
         def to_s
-            @text
+            @html
         end
     end
 
-    def handle_formatting(content_html, no_mutation_ranges)
-        # Split the content HTML into an array of bits. Put
-        # the bits that should not be formatted into the array with
-        # the NoFormatting class.
-        bits = []
+    def tokenize_facets(text)
+        # Defines the tokens.
+        tokens = []
+
+        # Go through each facet.
         last_end = 0
-        no_mutation_ranges.each_with_index do |range, index|
-            # Get the start and end of the range.
-            start, end_ = range
+        @facets.each do |facet|
+            # Get the start/end indexes.
+            start_index = facet['index']['byteStart']
+            end_index = facet['index']['byteEnd']
 
-            # Get the text.
-            text = content_html[start...end_]
+            # Set last end to this.
+            last_end = end_index
 
-            # Add the in-between text to bits.
-            inbetween = content_html[last_end...start]
-            bits << inbetween unless inbetween.empty?
+            # Get everything between this start index and the last end and push it.
+            between = text[last_end..start_index]
+            tokens.push(between) unless between.empty?
 
-            # Add the text to bits.
-            bits << NoFormatting.new(text) unless text.empty?
+            # Get the content for this.
+            content = text[start_index..end_index]
 
-            # Set last end to the end of the range.
-            last_end = end_
+            # Make the content XSS safe.
+            safe_content = ERB::Util.html_escape(content)
+
+            # Get the first feature.
+            feature = facet['features'].first
+            unless feature
+                tokens.push(content)
+                next
+            end
+
+            # Switch on the type.
+            case feature['$type']
+            when 'app.bsky.richtext.facet#mention'
+                url = "/profile/#{feature['did']}"
+                html = "<a href=\"#{ERB::Util.html_escape(url)}\">#{safe_content}</a>".html_safe
+            when 'app.bsky.richtext.facet#link'
+                url = feature['uri']
+
+                # Try to parse the link.
+                parsed, is_media = handle_spooky_url(url)
+
+                # If it was not parsed, just add as a token.
+                unless parsed
+                    tokens.push(content)
+                    next
+                end
+
+                # Build the HTML element.
+                html = "<a target=\"_blank\" href=\"#{ERB::Util.html_escape(url)}\">#{safe_content}</a>".html_safe
+
+                # If it is media, store it.
+                @last_media = url if is_media
+            else
+                tokens.push(content)
+                next
+            end
+
+            # Push the HTML.
+            tokens.push(FacetToken.new(html))
         end
 
-        # Set the remainder to everything after the last range.
-        bits << content_html[last_end..-1]
+        # Push the last bit of text.
+        tokens.push(text[last_end..-1]) unless last_end == text.length
 
-        # Go through the bits.
-        bits.each_with_index do |bit, index|
-            next if bit.is_a? NoFormatting
-
-            # Handle bold.
-            bit = bit.gsub(BOLD_TEXT, '<b>\1</b>')
-
-            # Handle italic.
-            bit = bit.gsub(ITALIC_TEXT, '<i>\1</i>')
-
-            # Handle underline.
-            bit = bit.gsub(UNDERLINE_TEXT, '<u>\1</u>')
-
-            # Handle strikethrough.
-            bit = bit.gsub(STRIKETHROUGH_TEXT, '<s>\1</s>')
-
-            # Replace the bit.
-            bits[index] = bit
-        end
-
-        # Transform the bits into a string.
-        bits = bits.map(&:to_s).join
-
-        # Return the bits.
-        bits.html_safe
-    end
-
-    def handle_newlines(content_html)
-        # Remove newlines from the start.
-        content_html = content_html.gsub(/\A\n+/, '')
-
-        # Replace multiple newlines with a couple newlines.
-        content_html = content_html.gsub(MANY_MULTIPLE_NEWLINES, '\n\n')
-
-        # Replace newlines with brs.
-        content_html = content_html.gsub(/\n/, '<br>')
-
-        # Return the content HTML.
-        content_html
+        # Return the tokens.
+        tokens
     end
 
     def handle_content_warnings
         # Try to match a content warning.
         match = CONTENT_WARNING.match(@text_content)
-        return '', '', @text_content unless match
+        return '', '', @text_content, 0 unless match
 
         # Get a santized version of the content warning.
         sanitized = ERB::Util.html_escape(match[1])
@@ -276,6 +260,6 @@ class SkeetRenderer
     </button></p>
 </form>
 <div style=\"display: none; margin-top: 1rem;\" id=\"#{@skeet_id}\" data-content-warning-target=\"content\">"
-        return html, closing, match.post_match
+        return html, closing, match.post_match, match[0].length
     end
 end
