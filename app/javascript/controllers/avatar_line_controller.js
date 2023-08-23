fd1d5438d9873c44491f1320501e0a6e36b9f730
Astrid Gealer <astrid@gealer.email>
2023-08-23 09:47:34 +0100

    Make it one svg for the page

--- app/javascript/controllers/avatar_line_controller.js
+++ app/javascript/controllers/avatar_line_controller.js
@@ -22,18 +22,18 @@ export default class extends Controller {
     this.darkMode = this.mediaQuery.matches
     this.darkHandler = () => {
       this.darkMode = this.mediaQuery.matches
-      this._destroySvg()
-      this._createSvg()
+      this._destroyLines()
+      this._createLines()
     };
     this.mediaQuery.addEventListener("change", this.darkHandler)
 
     // Create the SVG.
-    this._createSvg()
+    this._createLines()
 
     // Recreate the lines when the window resizes.
     this.resizeHandler = () => {
-      this._destroySvg()
-      this._createSvg()
+      this._destroyLines()
+      this._createLines()
     }
     window.addEventListener("resize", this.resizeHandler)
     window.addEventListener("scroll", this.resizeHandler)
@@ -43,22 +43,23 @@ export default class extends Controller {
   }
 
   disconnect() {
-    this._destroySvg()
+    this._destroyLines()
     this.mediaQuery.removeEventListener("change", this.darkHandler)
     window.removeEventListener("resize", this.resizeHandler)
     window.removeEventListener("scroll", this.resizeHandler)
   }
 
-  _createSvg() {
+  _createLines() {
     // Return if there are no pairs.
     if (this.pairs.length === 0) {
       return
     }
 
-    // Make a absolute positioned SVG.
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    // Get the SVG.
+    const svg = this.getWrappedSvg()
 
     // Build the lines.
+    this.lines = []
     for (const [top, bottom] of this.pairs) {
       // Get the absolute position of the bottom of the top.
       const topRect = top.getBoundingClientRect()
@@ -80,7 +81,27 @@ export default class extends Controller {
       line.setAttribute("stroke-width", "2")
       line.setAttribute("stroke-linecap", "round")
       svg.appendChild(line)
+
+      // Save the line.
+      this.lines.push(line)
     }
+  }
+
+  _destroyLines() {
+    for (const line of this.lines) {
+      line.remove()
+    }
+    this.lines = []
+  }
+
+  getWrappedSvg() {
+    // Try to get _wrapped_svg from the DOM.
+    let svg = document.getElementById("_wrapped_svg")
+    if (svg) return svg
+
+    // Make a SVG.
+    svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    svg.id = "_wrapped_svg"
 
     // Make the overflow visible.
     svg.setAttribute("overflow", "visible")
@@ -96,13 +117,6 @@ export default class extends Controller {
 
     // Add the SVG to the page.
     document.body.appendChild(wrapper)
-    this.svg = wrapper
-  }
-
-  _destroySvg() {
-    if (this.svg) {
-      this.svg.remove()
-      delete this.svg
-    }
+    return svg
   }
 }
