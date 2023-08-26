7406f5a1103cd19842e08eb0425594f57a09ea76
Astrid Gealer <astrid@gealer.email>
2023-08-26 01:09:24 +0100

    add opendyslexic support

---
+++ db/migrate/20230826000101_add_opendyslexic_to_user.rb
@@ -0,0 +1,5 @@
+class AddOpendyslexicToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :opendyslexic, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_180311) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_26_000101) do
   create_table "bluesky_user_editors", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "bluesky_user_id", null: false
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_180311) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "totp_secret"
+    t.boolean "opendyslexic"
     t.index "\"half_token\"", name: "index_users_on_half_token"
     t.index "\"user_token\"", name: "index_users_on_user_token"
     t.index ["email"], name: "index_users_on_email", unique: true
