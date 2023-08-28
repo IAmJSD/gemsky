2b929b6a9fcefafcf8a66130e02c1478e7965128
Astrid Gealer <astrid@gealer.email>
2023-08-28 03:17:03 +0100

    add patch to add user

--- app/components/input_component.rb
+++ app/components/input_component.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class InputComponent < ViewComponent::Base
-  def initialize(name:, label:, type: nil, value: nil, description: nil)
+  def initialize(name:, label:, type: nil, value: nil, description: nil, slim: false)
     @name = name
     @label = label
     @type = type
     @value = value
     @description = description
+    @slim = slim
   end
 
   def type
